39e70c49653e48df81c467dcf0f69e10f43808d6
vladdimitrov <32432611+vladdimitrov@users.noreply.github.com>
2017-12-03 18:58:30 +0200

    Add files via upload

---
+++ HornetWings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornetWings
+{
+    class HornetWings
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            decimal M1000 = decimal.Parse(Console.ReadLine());
+
+            int P = int.Parse(Console.ReadLine());
+            int restS = 5;
+            int wflapsS = 100;
+            int time = N / wflapsS;
+            int rest = N / P * 5;
+            int allTime = time + rest;
+
+            decimal distance = N * M1000/1000;
+
+
+            Console.WriteLine($"{distance:F2} m.");
+            Console.WriteLine($"{allTime} s.");
+
+
+
+        }
+    }
+}
